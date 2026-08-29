a50425d7c8848b42d80029f0a5cfb29a256efe38
deepak deore <deepakdeore5650@gmail.com>
2026-08-29 21:20:00 +0530

    Update successHandler.java

--- Back-End/src/main/java/com/ai/Resume/analyser/service/successHandler.java
+++ Back-End/src/main/java/com/ai/Resume/analyser/service/successHandler.java
@@ -54,7 +54,7 @@ public class successHandler implements AuthenticationSuccessHandler {
         ResponseCookie cookie = ResponseCookie.from("entrypasstoken", token).path("/").httpOnly(true).maxAge(20*24*60*60).sameSite("None").secure(true).build();
         // ResponseCookie cookie = ResponseCookie.from("entrypasstoken",token).path("/").httpOnly(true).maxAge(20*24*60*60).sameSite("Strict").secure(false).build();
         response.addHeader("Set-Cookie",cookie.toString());
-        response.sendRedirect("https://localservice-provider-1.onrender.com/");
+        response.sendRedirect("https://careerpilot-ai-1-cgrw.onrender.com/");
 
 
 
